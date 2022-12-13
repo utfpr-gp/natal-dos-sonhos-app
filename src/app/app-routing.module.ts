0564426e998fd61c401ed9d17397e4fbaee44102
Ligia Falcao <lih.falcao@hotmail.com>
2022-12-13 00:03:40 -0300

    Sign up and login pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,17 @@ import { NgModule } from '@angular/core';
 import { LandPageComponent } from './components/visitor/land-page/land-page.component';
 import { PageNotFoundComponent } from './components/visitor/page-not-found/page-not-found.component';
 import { LetterPageComponent } from './components/visitor/letter-page/letter-page/letter-page.component';
+import { LoginComponent } from './components/visitor/login/login.component';
+import { SignUpComponent } from './components/visitor/sign-up/sign-up.component';
 
 const routes: Routes = [
   { path: 'inicio', component: LandPageComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cadastre-se', component: SignUpComponent },
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent },
   { path: 'cartas/:id', component: LetterPageComponent },
+
 ];
 
 @NgModule({
